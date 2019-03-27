9151b44230fed149fe0bb872b70bb6827bed16e6
valcaza <victor.alcazar@lts-beratung.de>
2019-03-27 15:35:51 +0100

    Added argument for output directory

--- cli.js
+++ cli.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 'use strict';
+const fs = require('fs');
 const meow = require('meow');
 const gineta = require('.');
 
 const cli = meow(`
-  Usage
-    gineta [directory]
+	Usage
+		gineta [directory]
 
 	Arguments
 		directory: Specify the directory to get the git information from
-  Examples
-    $ gineta
+	Examples
+		$ gineta
 // => {
 				"date": "Wed Mar 27 15:11:39 CET 2019",
 				"hostname": "vikepic.computer",
@@ -25,8 +26,18 @@ const cli = meow(`
 				"gitChanges": "M cli.js
 			 M index.js
 			 M readme.md"
-	   }
+		 }
 `);
 
-const jsonResult = JSON.stringify(gineta(cli.input[0] || '.'), null, 2);
-console.log(jsonResult.replace(/\\n/g, '\n'));
+const originPath = cli.input[0];
+
+const jsonResult = gineta(originPath || '.');
+const jsonString = JSON.stringify(jsonResult, null, 2);
+const jsonReplaced = jsonString.replace(/\\n/g, '\n');
+
+const destinationPath = cli.input[1];
+if (destinationPath) {
+	fs.writeFileSync(destinationPath, jsonReplaced);
+} else {
+	console.log(jsonReplaced);
+}
